18ea91db2f20fce0f6670480bbaf16988951a233
sidosho <sksosho@yahoo.in>
2018-07-29 14:46:28 +0530

    added structure of admin class.

--- admin.py
+++ admin.py
@@ -1,6 +1,25 @@
-class ClassName(object):
-    """docstring for ."""
-    def __init__(self, arg):
-        super(, self).__init__()
-        self.arg = arg
-        
+# class ClassName(object):
+#     """docstring for ."""
+#     def __init__(self, arg):
+#         super(, self).__init__()
+#         self.arg = arg
+
+class Admin:
+    admin_pass='root'
+    def custDetail(self,cust):
+
+        pass
+    def  pending_orders(self):
+        pass
+    def updateOrderStatus(self,order):
+        pass
+    def stockOrder(self,manf,med,qty):
+        pass
+    def UpdateInventoy(self,med):
+        pass
+    def __init__(self,aid):
+        super(self,aid)
+        pass
+    @staticmethod
+    def login(mail,passw):
+        pass
